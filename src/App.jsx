0d5464737b353f88218edaf24ae8b10f77196739
igprad01 <igustiputra99@gmail.com>
2024-05-04 11:53:15 +0700

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 
-function Button() {
-  return(
-    <button className='px-5 py-5 bg-red-800'>ini adalah button</button>
-  );
+
+const ButtonA = (props) => {
+  return (
+    <button className={`px-10 py-10 ${props.variant}`}>{props.Teks}</button>
+  )
 }
 
-function Teks() {
+const Teks = () => {
+  const props = {
+    nama: "AAAAAAAA",
+    jurusan: "informatika"
+  };
   return(
-    <h1>hello world</h1>
-  );
+    <div>
+      <h1 className={`font-mono text-xl`}>{props.nama}, {props.jurusan}</h1>
+      <h2>{props.nama}</h2>
+    </div>
+  )
 }
+
+
 
 
 function App() {
   return (
     <div className='flex justify-center items-center mt-24 mr-10'>
       <div>
-        <Button></Button>
-        <Teks></Teks>
-        <Teks></Teks>
-        <Teks></Teks>
+        <ButtonA variant = "bg-red-700" Teks = "ini adalah teks"/>
+        <ButtonA variant = "bg-blue-200" />
+        <Teks />
       </div>
     </div>
   )
