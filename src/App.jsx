6d2fca2cf82bee64efe54f94c66fd3733ebdf802
igprad01 <igustiputra99@gmail.com>
2024-05-07 21:19:14 +0700

    form on progress

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,10 @@ function App() {
     <div className='flex justify-center items-center mt-24 mr-10'>
       <div>
         <Button color = "bg-red-200"/>
-        <Button color = "bg-white"/>
-        <Button color = "bg-red-200"/>
-        <Teks />
-        <Teks />
-        <Picture ukuran = "500px"/>
       </div>
     </div>
   )
 }
 
-// jadi buat form setelah pekerjaan Proposal Kerja Praktek
 
 export default App
---
+++ src/components/ELements/button/button.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ButtonA = (props) => {
+    return (
+      <button className={`px-10 py-10 ${props.color}`}></button>
+    )
+  }
+
+  export default ButtonA
