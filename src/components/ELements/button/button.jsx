adb01b217b088228e910c19e152e575853115f76
igprad01 <igustiputra99@gmail.com>
2024-05-15 20:14:03 +0700

    add form

--- src/components/ELements/button/button.jsx
+++ src/components/ELements/button/button.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const ButtonA = (props) => {
   
     return (
-      <button className={`px-12 py-5 ${props.color}`}>Submit</button>
+      <button className={`px-12 py-5 ${props.color} ${props.textColor}`}>Submit</button>
     )
   }
 
--- src/components/ELements/form/form.jsx
+++ src/components/ELements/form/form.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ButtonA from "../button/button";
 
+
+
 const input = () => {
     return (
-        <form action="">
-            <div>
-                
-            </div>
-            <ButtonA color = 'bg-black'/>
-        </form>
+      <form action="">
+        <div className="">
+
+        </div>
+      </form>
     )
 }
 
