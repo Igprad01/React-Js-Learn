c0709d247c384a4b914c532146d318a887f4c8d0
igprad01 <igustiputra99@gmail.com>
2024-07-24 09:35:57 +0700

    on progress navbar responsive

--- src/components/Layouts/navbar.jsx
+++ src/components/Layouts/navbar.jsx
@@ -4,9 +4,8 @@ import ListNav from "../ELements/ListNav/List";
 import Search from "../ELements/Pencarian/Pencarian";
 
 const Navbar = () => {
-
     const [isOpen, setIsOpen] = useState(false);
-
+    
     const ToggleMenu = () => {
         setIsOpen(!isOpen);
     };
@@ -15,15 +14,15 @@ const Navbar = () => {
         <nav className="w-full bg-blue-700 p-10">
             <div className="container mx-auto flex justify-between text-2xl capitalize text-white">
                 <LogoNav title = 'Indo Movies'  />
-                <div className="hidden md:flex items-center space-x-4">
+                <div className="hidden md:flex items-center space-x-14">
                 <Search />  
                 <ListNav />
                 </div>
-                <div >
-                    <button onClick={ToggleMenu}>
+                <div className="md:hidden flex items-center">
+                    <button onClick={ToggleMenu} className="focus: outline-none">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-                        </svg>
+                    </svg>
                     </button>
                 </div>
             </div>
@@ -36,6 +35,5 @@ const Navbar = () => {
         </nav>
     )
 }
-//  responsive navbar hamburger 
 
 export default Navbar
