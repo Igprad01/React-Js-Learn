a79ed7a54b98e1c63629a879927d16767919d256
igprad01 <igustiputra99@gmail.com>
2024-07-25 15:33:09 +0700

    add hover to list navbar

--- src/components/ELements/ListNav/List.jsx
+++ src/components/ELements/ListNav/List.jsx
@@ -1,12 +1,11 @@
-import { list } from "postcss";
 import React from "react";
 
 const ListNav = (props) => {
     const List = ['home','How to Download','other'];
     return (
-        <ul className=" flex justify-end ">
+        <ul className=" flex justify-end">
             {List.map((list) => (
-                <a href="#" className={`mr-10 ${'props.margin'}`}>{list}</a>
+                <a href="#" className={`mr-10 ${'props.margin'} ${props.animation} `}>{list}</a>
             ))}
         </ul>
     )
