adbd139fae55c2aeff6a10000eedd91f75479289
igprad01 <igustiputra99@gmail.com>
2024-07-25 15:34:07 +0700

    fix layout hero section

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Heading1 from "../ELements/Heading1/H1";
-import Button from "../ELements/Button/Button";
-
 
 const HeroSection = () => {
+
+    const BackgroundImg = ('');
+
     return (
         <div className="w-full bg-cyan-700 px-64 py-64 ">
            
