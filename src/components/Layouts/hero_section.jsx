bfa995409796c0efca06e0f9eb0af06eec4aa45b
igprad01 <igustiputra99@gmail.com>
2024-07-29 17:05:49 +0700

    fix hero_section desktop

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import Heading1 from "../ELements/Heading1/H1";
 import Heading2 from "../ELements/Heading2/H2";
 
-
 const HeroSection = () => {
-
     return (
-        <div className=" relative w-full bg-blue-500 bg-hero-section bg-cover py-32 px-32 md:py-58 lg:py-72 xl:py-96">
-            <div className="absolute inset-0 bg-black opacity-40">
-                <div className="text-5xl text-center ">
-                    <Heading1 />
-                    <Heading2 />
+        <div className="relative w-full bg-blue-500 bg-hero-section bg-cover py-24 px-32 md:py-58 lg:py-72 xl:py-96 flex items-center justify-center">
+            <div className="absolute inset-0 bg-black opacity-40 flex items-center justify-center">
+                <div className="text-center font-bold">
+                    <Heading1 text="MOVIES WEB" styling="text-white text-5xl" />
+                    <Heading2 text = "lorem ipsum" styling = 'text-white text-xl' />
                 </div>
             </div> 
         </div>
-    )
+    );
 }
 
-
-export default HeroSection
+export default HeroSection;
