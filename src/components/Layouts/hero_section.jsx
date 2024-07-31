a67de006e20148f5f565ff403be85b4fa9149ed4
igprad01 <igustiputra99@gmail.com>
2024-07-31 22:10:18 +0700

    delete background image

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Heading1 from "../ELements/Heading1/H1";
 import Heading2 from "../ELements/Heading2/H2";
+import Search from "../ELements/Pencarian/Pencarian";
 
 const HeroSection = () => {
     return (
-        <div className="relative w-full bg-blue-500 bg-hero-section bg-cover py-24 px-32 md:py-58 lg:py-72 xl:py-96 flex items-center justify-center">
+        <div className="relative w-full bg-white bg-cover py-24 px-32 md:py-58 lg:py-72 xl:py-96 flex items-center justify-center">
             <div className="absolute inset-0 bg-black opacity-40 flex items-center justify-center">
                 <div className="text-center font-bold">
                     <Heading1 text="MOVIES WEB" styling="text-white text-5xl" />
                     <Heading2 text = "lorem ipsum" styling = 'text-white text-xl' />
+                    <Search padding = 'py-2 pr-10'/>
                 </div>
             </div> 
         </div>
