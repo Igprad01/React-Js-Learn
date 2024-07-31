81862f5b15712ae97fa713c73f6987fcd69a9162
igprad01 <igustiputra99@gmail.com>
2024-08-01 00:28:02 +0700

    delete background img

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -5,8 +5,8 @@ import Search from "../ELements/Pencarian/Pencarian";
 
 const HeroSection = () => {
     return (
-        <div className="relative w-full bg-white bg-cover py-24 px-32 md:py-58 lg:py-72 xl:py-96 flex items-center justify-center">
-            <div className="absolute inset-0 bg-black opacity-40 flex items-center justify-center">
+        <div className="relative w-full bg-black py-24 px-32 md:py-58 lg:py-72 xl:py-96 flex items-center justify-center">
+            <div className="absolute flex items-center justify-center">
                 <div className="text-center font-bold">
                     <Heading1 text="MOVIES WEB" styling="text-white text-5xl" />
                     <Heading2 text = "lorem ipsum" styling = 'text-white text-xl' />
