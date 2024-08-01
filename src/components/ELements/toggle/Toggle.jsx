0aa06dea9c6bf19c2eaa36577d503edd9bd93798
igprad01 <igustiputra99@gmail.com>
2024-08-01 22:54:18 +0700

    add toggle on off

--- src/components/ELements/toggle/Toggle.jsx
+++ src/components/ELements/toggle/Toggle.jsx
@@ -1,5 +1,17 @@
 import { useState } from "react"
 
 const Toggle = () => {
-    
+    const [isToggle, setIstoggle] = useState(false)
+
+    const handlebutton = () => {
+        setIstoggle(!isToggle)
+    }
+
+    return (
+        <button onClick={handlebutton}>
+            {isToggle ? 'ON' : 'OFF'}
+        </button>
+    )
 }
+
+export default Toggle
