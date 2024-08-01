c690b8a08deb6aa2a35de3bed944cc7422621276
igprad01 <igustiputra99@gmail.com>
2024-08-01 23:08:00 +0700

    fix responsive navbar

--- src/components/Layouts/navbar.jsx
+++ src/components/Layouts/navbar.jsx
@@ -12,12 +12,12 @@ const  Navbar = () => {
     
     return (
         <nav className="w-full p-5 shadow-lg">
-            <div className="container mx-auto flex justify-between text-xl capitalize text-black">
-                <div className="hidden md:flex items-center space-x-10">
+            <div className="container mx-auto text-xl capitalize text-black">
+                <div className="hidden justify-between md:flex items-center space-x-10">
                     <ListNav animation = 'hover:underline'/> 
                     <Toggle />
                 </div>
-                <div className="md:hidden flex items-center bg-white">
+                <div className=" justify-between mr-5 md:hidden flex items-center bg-white">
                     <button onClick={ToggleMenu} className="focus: outline-none">
                     <svg viewBox="0 0 100 80" width="20" height="20">
                         <rect width="100" height="20"></rect>
@@ -25,11 +25,12 @@ const  Navbar = () => {
                         <rect y="60" width="100" height="20"></rect>
                     </svg>
                     </button>
+                    <Toggle />
                 </div>
             </div>
             {isOpen && (
                 <div className="mt-5 font-bold text-black text-base capitalize">
-                    <div className="mr-2 ml-1">
+                    <div className="ml-1">
                     <ListNav />
                     </div>
                 </div>
