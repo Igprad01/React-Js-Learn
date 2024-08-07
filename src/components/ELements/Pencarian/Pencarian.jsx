8a6b39ffdd976d45248d23f22548316a8a21851c
igprad01 <igustiputra99@gmail.com>
2024-08-07 13:41:10 +0700

    custom styling

--- src/components/ELements/Pencarian/Pencarian.jsx
+++ src/components/ELements/Pencarian/Pencarian.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Search = (props) => {
     return (
-        <input type="search" name="#" id="#" placeholder="Search a for movie" className={`block w-96 text-black border outline-0 ml-50 rounded-md pl-5 ${props.padding}`} />
+        <input type="search" name="#" id="#" placeholder="Search a for movie" className={`block w-full text-black border-4 ml-50 rounded-md pl-5 mt-5 ${props.padding} hover: outline-8`} />
     )
     
 }
