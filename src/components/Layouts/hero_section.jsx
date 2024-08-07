d49531af33f2adddc721030328bb4356e2241d6f
igprad01 <igustiputra99@gmail.com>
2024-08-07 13:42:31 +0700

    finishing hero section

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Heading1 from "../ELements/Heading1/H1";
-import Heading2 from "../ELements/Heading2/H2";
 import Search from "../ELements/Pencarian/Pencarian";
+
 
 const HeroSection = () => {
     return (
-        <div className="relative w-full bg-black py-24 px-32 md:py-58 lg:py-72 xl:py-96 flex items-center justify-center">
+        <div className="relative w-full py-24 px-32 md:py-58 lg:py-72 flex items-center justify-center">
             <div className="absolute flex items-center justify-center">
-                <div className="text-center font-bold">
-                    <Heading1 text="MOVIES WEB" styling="text-white text-5xl" />
-                    <Heading2 text = "lorem ipsum" styling = 'text-white text-xl' />
-                    <Search padding = 'py-2 pr-10'/>
+                <div className="text-center">
+                    <Heading1 text='enjoy exploring movies on our website' styling="text-black font-bold text-7xl capitalize" />
+                    <Search padding = 'py-2 pr-10 mt-10'/>
                 </div>
             </div> 
         </div>
