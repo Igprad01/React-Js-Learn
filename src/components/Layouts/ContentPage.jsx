5d00f223da7a61aa970d621bd8c5e0eba3320b9f
igprad01 <igustiputra99@gmail.com>
2024-08-08 08:16:04 +0700

    add homepage

---
+++ src/components/Layouts/ContentPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading1 from '../ELements/Heading1/H1';
+
+const ContentPage = () => {
+    return (
+        <div className="block">
+        <Heading1 text = 'ini adalah judul content' styling = 'text-black   ' />
+        </div>
+    )
+}
+
+export default ContentPage
