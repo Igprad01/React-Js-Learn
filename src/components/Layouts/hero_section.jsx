adeac5ed2a800ace0768e9f7e0b2c5ec809c65dc
igprad01 <igustiputra99@gmail.com>
2024-08-08 08:08:08 +0700

    fix responsive mobile hero_section

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <div className="relative w-full py-24 px-32 md:py-58 lg:py-72 flex items-center justify-center">
             <div className="absolute flex items-center justify-center">
                 <div className="text-center">
-                    <Heading1 text='enjoy exploring movies on our website' styling="text-black font-bold text-7xl capitalize" />
+                    <Heading1 text='enjoy exploring movies on our website' styling="text-black font-bold text-xl capitalize md:text-5xl lg:text-5xl" />
                     <Search padding = 'py-2 pr-10 mt-10'/>
                 </div>
             </div> 
