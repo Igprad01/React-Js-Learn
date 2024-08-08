9ea9fb8c0df96de2b8caf99a8569aa2fc777377c
igprad01 <igustiputra99@gmail.com>
2024-08-08 18:35:45 +0700

    fix design layout contentPage

--- src/components/Layouts/ContentPage.jsx
+++ src/components/Layouts/ContentPage.jsx
@@ -3,8 +3,9 @@ import Heading1 from '../ELements/Heading1/H1';
 
 const ContentPage = () => {
     return (
-        <div className="block">
-        <Heading1 text = 'ini adalah judul content' styling = 'text-black   ' />
+        <div className="container bg-slate-200  mx-auto p-5 rounded-lg shadow-lg">
+            <Heading1 text = 'ini adalah teks' />
+            
         </div>
     )
 }
