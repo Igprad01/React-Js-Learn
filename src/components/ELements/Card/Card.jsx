0904f6ecd1e277f3ec51863977953baec81020ad
igprad01 <igustiputra99@gmail.com>
2024-08-09 09:45:06 +0700

    on progress card content page

--- src/components/ELements/Card/Card.jsx
+++ src/components/ELements/Card/Card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <div>
+        <div className={``}>
             {/* on progress */}
         </div>
     )
