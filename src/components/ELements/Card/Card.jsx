d56fac448956700db23296a42c132fd11909fcc1
igprad01 <igustiputra99@gmail.com>
2024-08-12 22:35:02 +0700

    delete useeffect and usestate -> add props and default return components

--- src/components/ELements/Card/Card.jsx
+++ src/components/ELements/Card/Card.jsx
@@ -1,15 +1,13 @@
-import React from "react";
+import React from 'react';
 import Heading1 from './../Heading1/H1';
-
-// get api nya nanti disini
-
 
 const Card = (props) => {
     return (
         <div className={`box-border py-5 px-5 bg-zinc-50`}>
-            <Heading1 text = 'ini adalah card' />
+            <Heading1 text={props.movie.title} />
+            <Heading1 text = {props.movie.year} />
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
