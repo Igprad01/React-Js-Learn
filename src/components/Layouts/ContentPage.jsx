55daac198719a7066fafee513f6572c2a56fdb50
igprad01 <igustiputra99@gmail.com>
2024-08-12 17:40:37 +0700

    uji coba fetch api content

--- src/components/Layouts/ContentPage.jsx
+++ src/components/Layouts/ContentPage.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading1 from '../ELements/Heading1/H1';
 import Card from "../ELements/Card/Card";
+import { useEffect } from "react";
+import GetMovie  from "../../api/MovieAPi"
 
 const ContentPage = () => {
+    const [moviepage, setmoviepage] = useState(null);
+
+    useEffect(()=> {
+        GetMovie()
+        });
+
     const Numbercard = 10;
 
     const ArrayCard = Array.from({length : Numbercard});
+
     return (
         <div className={`container bg-slate-200  mx-auto p-5 rounded-lg shadow-lg`}>
             <Heading1 text = 'ini adalah teks' />
             <div className={`w-full grid grid-cols-5 gap-5 mb-7`}>
                 {ArrayCard.map((_, dat)=>(
                     <Card key = {dat} />
-                ))}
+                ))};
             </div>
         </div>
     )
