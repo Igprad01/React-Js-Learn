f933cd181269e699c72fd31d37bccf0d6168bcbf
igprad01 <igustiputra99@gmail.com>
2024-08-13 10:03:37 +0700

    fix API and responsive web columns

--- src/components/Layouts/ContentPage.jsx
+++ src/components/Layouts/ContentPage.jsx
@@ -5,7 +5,7 @@ import GetMovie from "../../api/MovieAPi";
 
 const ContentPage = () => {
     const [movies, setmoviepage] = useState([]);
-    const Numbercard = 10; 
+    const Numbercard = 30; 
 
     useEffect(() => {
         const Moviedata = async () => {
@@ -17,9 +17,9 @@ const ContentPage = () => {
     },[])
  
     return (
-        <div className={`container bg-slate-200 mx-auto p-5 rounded-lg shadow-lg`}>
-            <Heading1 text='ini adalah teks' />
-            <div className={`w-full grid grid-cols-5 gap-5 mb-7`}>
+        <div className={`container bg-slate-200 mx-auto p-5 rounded-lg shadow-lg `}>
+            <Heading1 text='movies list' styling = 'text-center font-bold text-4xl uppercase' />
+            <div className={`w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-7 mt-10`}>
                 {movies.slice(0, Numbercard).map((movie, index) => (
                     <Card key={index} movie={movie} />
                 ))}
@@ -29,3 +29,4 @@ const ContentPage = () => {
 }
 
 export default ContentPage;
+
