67f6d2fa3b8cc82ae233110b8341bac533b45f26
igprad01 <igustiputra99@gmail.com>
2024-08-13 10:03:08 +0700

    add fetch poster,title, year and custom styling responsive card

--- src/components/ELements/Card/Card.jsx
+++ src/components/ELements/Card/Card.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Heading1 from './../Heading1/H1';
+import Gambar from '../Image/Image';
 
 const Card = (props) => {
     return (
-        <div className={`box-border py-5 px-5 bg-zinc-50`}>
-            <Heading1 text={props.movie.title} />
-            <Heading1 text = {props.movie.year} />
+        <div className={`box-border py-10 px-10  bg-zinc-50 rounded-xl `}>
+           <div className='flex justify-center items-center'> 
+           <Gambar imgLink = {props.movie.poster} height = '500px'/>
+           </div>
+            <Heading1 text={props.movie.title} styling = 'font-bold text-center mt-5 text-2xl ' />
+            <Heading1 text = {props.movie.year} styling = 'text-center mt-5 text-xl' />
         </div>
     );
 };
