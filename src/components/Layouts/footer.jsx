2a965652bdae543c139b06939358e5d29ef14d7b
igprad01 <igustiputra99@gmail.com>
2024-08-13 10:00:53 +0700

    add footer

---
+++ src/components/Layouts/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading2 from "../ELements/Heading2/H2";
+
+
+
+const Footer = () => {
+    return (
+        <div className=" px-10 py-10 mt-5 text-center">
+            <Heading2 text ='by Igprad' />
+        </div>
+    )
+}
+
+export default Footer
