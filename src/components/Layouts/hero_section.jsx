0e3518f50c64b8eb949000df792233e59e4ba61a
igprad01 <igustiputra99@gmail.com>
2024-08-23 11:44:57 +0700

    on progress dark mode to hero_Section

--- src/components/Layouts/hero_section.jsx
+++ src/components/Layouts/hero_section.jsx
@@ -3,17 +3,17 @@ import Heading1 from "../ELements/Heading1/H1";
 import Search from "../ELements/Pencarian/Pencarian";
 
 
-const HeroSection = () => {
+const HeroSection = ({isDarkMode}) => {
     return (
-        <div className="relative w-full py-24 px-32 md:py-58 lg:py-72 flex items-center justify-center">
-            <div className="absolute flex items-center justify-center">
-                <div className="text-center">
-                    <Heading1 text='enjoy exploring movies on our website' styling="text-black font-bold text-xl capitalize md:text-5xl lg:text-5xl" />
-                    <Search padding = 'py-2 pr-10 mt-10'/>
+        <div className={`relative w-full py-24 px-32 md:py-58 lg:py-72 flex items-center justify-center ${isDarkMode ? `bg-gray-800 text-white` : `bg-white text-black`}`}>
+            <div className={`absolute flex items-center justify-center`}>
+                <div className={`text-center`}>
+                    <Heading1 text='enjoy exploring movies on our website' styling={`text-black font-bold text-xl capitalize md:text-5xl lg:text-5xl`} />
+                    <Search padding = {`py-2 pr-10 mt-10`}/>
                 </div>
             </div> 
         </div>
     );
 }
 
-export default HeroSection;
+export default HeroSection
