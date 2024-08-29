7ebba7426b511f0fbb0a965b7d00b3606d990373
igprad01 <igustiputra99@gmail.com>
2024-08-29 20:13:01 +0700

    add props toggle menu dark mode and light mode

--- src/pages/template.jsx
+++ src/pages/template.jsx
@@ -2,14 +2,21 @@ import ContentPage from "../components/Layouts/ContentPage"
 import Footer from "../components/Layouts/footer"
 import HeroSection from "../components/Layouts/hero_section"
 import Navbar from "../components/Layouts/navbar"
+import { useState } from "react"
 
 const Template = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggletheme = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
     return (
         <body>
-            <Navbar />
-            <HeroSection />
-            <ContentPage />
-            <Footer />
+            <Navbar toggleTheme={toggletheme} isDarkMode={isDarkMode} />
+            <HeroSection isDarkMode={isDarkMode}  />
+            <ContentPage isDarkMode={isDarkMode} />
+            <Footer isDarkMode={isDarkMode} />
         </body>
     )
 }
